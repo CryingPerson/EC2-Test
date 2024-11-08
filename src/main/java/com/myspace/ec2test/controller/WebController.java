101fda1488208f3b4dadba1a1bc952e937c80f5c
CryingPerson <opnice12@naver.com>
2024-11-08 17:44:00 +0900

    [add] gradle 설정추가

--- src/main/java/com/myspace/ec2test/controller/WebController.java
+++ src/main/java/com/myspace/ec2test/controller/WebController.java
@@ -1,16 +1,20 @@
 package com.myspace.ec2test.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@Slf4j
 @RestController
 public class WebController {
 
     @GetMapping("api/v1")
     public Map<String, String> chech() {
-
+        log.info(" 성공 축하 ");
+        log.error(" 실패 실패 ");
+        log.warn(" 조심 조심 ");
         return Map.of("hello", "world");
     }
 }
